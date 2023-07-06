feecc8d3d35403a571fbf264308d03b00d7d0c1b
justforfun0906 <jimmy0906abc@gmail.com>
2023-07-06 23:28:54 +0800

    bruh

--- webscrapper.py
+++ webscrapper.py
@@ -7,19 +7,19 @@ base = 'https://www.ptt.cc/bbs/C_Chat/index'
 out_file = 'c_chat_title.txt'
 
 ind = 17642
-for i in range(ind,17500,-1):    
+for i in range(ind,17600,-1):    
     the_url = base+str(i)+'.html'
     print("url = %s"% the_url)
     htmlfile = requests.get(the_url)
     if htmlfile.status_code == requests.codes.ok :
         print("requested successfully")
         objSoup = BeautifulSoup(htmlfile.text, 'html5lib')
-        objTitle = objSoup.find_all("div", {"class":"title"})
+        objTitle = objSoup.find_all("div", class_="title")
         file_obj = open(out_file,'a',encoding='utf_16', errors='ignore')
         file_obj.write("url = "+ the_url)
         for data in objTitle:
             #print(data.text)
-            file_obj.write(data.text + '\n')
+            file_obj.write(data.text)
         file_obj.close()        
     else :
         print("failed")

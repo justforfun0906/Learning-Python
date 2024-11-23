1a89de4662bded6f99238017f44211476bf89a9f
justforfun0906 <jimmy0906abc@gmail.com>
2024-11-24 03:24:28 +0800

    w9 done

--- weekly_checkpoint/w11.py
+++ weekly_checkpoint/w11.py
@@ -1,4 +1,7 @@
 import os
+class  Record:
+class Records:
+class Category:
 def is_valid_integer(s):
     try:
         int(s)
@@ -29,12 +32,13 @@ def initialize():
             assert is_valid_integer(balance_str), 'balance is not a number'
             initial_money = int(balance_str)
             for line in file_li[1:]:
-                desc, amount = line.split()
+                cate, desc, amount = line.split()
+                cate = cate.strip()
                 desc = desc.strip()
                 amount = amount.strip()
                 #make sure amount is a number
                 assert is_valid_integer(amount), 'amount is not a number'
-                records.append((desc, int(amount)))
+                records.append((cate, desc, int(amount)))
     except FileNotFoundError:
         print("record.txt file not found.")
         initial_money = int(input("How much money do you have? "))
@@ -48,17 +52,31 @@ def initialize():
     except Exception as e:
         #can't read the file
         initial_money = handle_balance_input()
-        
+def initialize_categories():
+    global category_list, flatten_category_list
+    category_list = ['expense', ['food', ['meal', 'snack', 'drink'], 'transportation', ['bus', 'railway']], 'income', ['salary', 'bonus']]
+    flatten_category_list = flatten_list(category_list)
+def view_categories(category_list, level=0):
+    #recursively print the categories
+    for child in category_list:
+        if isinstance(child, list):
+            view_categories(child, level+1)
+        else:
+            print('  '*level +'-'+ child)
 def add(records, initial_money):
-    in_str = input("Add some expense or income records with description and amount (format: 'item amount'): ")
+    in_str = input("Add some expense or income records with category, description and amount (format: 'category item amount'): ")
     in_list = in_str.split(', ')
     for item in in_list:
         try:
-            desc = item.split()[0]
-            amount = item.split()[1]
+            cate = item.split()[0]
+            assert cate in flatten_category_list, f"Invalid category: '{cate}'. Please use the categories in the list. \nFail to add a record."
+            desc = item.split()[1]
+            amount = item.split()[2]
             amount = int(amount)
-            records.append((desc, amount))
+            records.append((cate, desc, amount))
             initial_money += amount
+        except AssertionError as e:
+            print(e)
         except IndexError:
             #item can't be split into two parts
             print(f"Invalid format for record: '{item}'. Please use the format \"item amount\" like \"breakfast -50\". \nFail to add a record.")
@@ -66,7 +84,13 @@ def add(records, initial_money):
             #int() can't convert the amount to an integer
             print(f"Invalid value for money: '{amount}'. Please use an integer. \nFail to add a record.")
     return initial_money
-    
+def view(records):
+    print("Category        Description          Amount")
+    print("=============== ==================== ======")
+    for cate, desc, amount in records:
+        print(f"{cate:<15} {desc:<20} {amount:>5}")
+    print("===========================================")
+    print("Now you have {} dollars.".format(initial_money))
 def delete(records, initial_money):
     try:
         assert len(records) > 0, 'No record to delete.'
@@ -99,38 +123,73 @@ def delete(records, initial_money):
             #if there's no such record with the amount
             print(f"No record with description '{in_str}' and amount '{amt}'.")
     return initial_money
+def flatten_list(li):
+    if type(li) == list:
+        ret_val = []
+        for item in li:
+            ret_val.extend(flatten_list(item))
+        return ret_val
+    else:
+        return [li]
+def find_subcate(category, categories):
+    if type(categories) == list:
+        for v in categories:
+            result = find_subcate(category, v)
+            if result == True:
+                index = categories.index(v)
+                if index + 1 < len(categories) and \
+                        type(categories[index + 1]) == list:
+                    return flatten_list(categories[index:index + 2])
+                else:
+                    # return only itself if no subcategories
+                    return [v]
+            if result != []:
+                return result
+    return True if categories == category else []
+def find(records):
+    in_str = input("Please enter the description of the record you want to find:").strip()
+    subcate = find_subcate(in_str, category_list)
+    #output
+    print("Category        Description          Amount")
+    print("=============== ==================== ======")
+    for cate, desc, amount in records:
+        if cate in subcate:
+            print(f"{cate:<15} {desc:<20} {amount:>5}")
+    print("===========================================")
+    print("Now you have {} dollars.".format(initial_money))
 def save():
     global initial_money
     global records
     #write the initial_money and records to a file
     file_path = os.path.join(os.path.dirname(__file__), 'record.txt')
-    fh = open(file_path, 'w')
-    #write the initial_money first then the records
-    fh.write(str(initial_money) + '\n')
-    for desc, amount in records[:-1]:
-        fh.write(desc + ' ' + str(amount) + '\n')
-    #the last record should not have a new line
-    if(len(records)): fh.write(records[-1][0] + ' ' + str(records[-1][1]))
-    fh.close()
+    with open(file_path, 'w') as fh:
+        #write the initial_money first then the records
+        fh.write(str(initial_money) + '\n')
+        for cate, desc, amount in records[:-1]:
+            fh.write(f"{cate} {desc} {amount}\n")
+        #the last record should not have a new line
+        if len(records):
+            fh.write(f"{records[-1][0]} {records[-1][1]} {records[-1][2]}")
     
 initialize()
-command = input("What do you want to do? (add / view / delete / exit)?")
-legal_commands = ['add', 'view', 'delete', 'exit']
+initialize_categories()
+print(flatten_category_list)
+command = input("What do you want to do? (add / view / delete / view categories / find / exit )?")
+legal_commands = ['add', 'view', 'delete', 'exit', 'view_categories', 'find']
 while True:
     if command == 'add':
         initial_money = add(records, initial_money)
     elif command == 'view':
-        print("Description          Amount")
-        print("==================== ======")
-        for desc, amount in records:
-            print(f"{desc:<20} {amount:>5}")
-        print("==================== ======")
-        print("Now you have {} dollars.".format(initial_money))
+        view(records)
     elif command == 'delete':
         initial_money=delete(records, initial_money)
     elif command == 'exit':
         break
+    elif command == 'view categories':
+        view_categories(category_list)
+    elif command == 'find':
+        find(records)
     else:
         print("Invalid command. Try again.")
-    command = input("What do you want to do? (add / view / delete / exit)?")
+    command = input("What do you want to do? (add / view / delete / view categories / find /exit)?")
 save()
